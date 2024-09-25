afbd8b9dabd0c998d05fa5ca37c808512a9b21ed
Ly Phu <phuly@bravesoft.com.vn>
2024-09-25 15:58:42 +0700

    init

--- src/views/QRView.vue
+++ src/views/QRView.vue
@@ -17,7 +17,7 @@
 
 <script lang="ts" setup>
 import { onMounted, ref } from "vue"
-import { Html5QrcodeScanner } from "html5-qrcode"
+import { Html5QrcodeScanner, Html5Qrcode } from "html5-qrcode"
 import { defineEmits } from 'vue'
 
 const emit = defineEmits(['result'])
@@ -38,12 +38,34 @@ onMounted(() => {
 })
 
 const initCamera = () => {
-    const config = {
-        fps: fps.value,
-        qrbox: qrbox.value,
-    };
-    const html5QrcodeScanner = new Html5QrcodeScanner('qr-code-full-region', config);
-    html5QrcodeScanner.render(onScanSuccess);
+    Html5Qrcode.getCameras().then(devices => {
+        /**
+         * devices would be an array of objects of type:
+         * { id: "id", label: "label" }
+         */
+        if (devices && devices.length) {
+            var cameraId = devices[0].id;
+            const html5QrCode = new Html5Qrcode(/* element id */ "qr-code-full-region");
+            html5QrCode.start(
+                cameraId,
+                {
+                    fps: 10,    // Optional, frame per seconds for qr code scanning
+                    qrbox: { width: 250, height: 250 },  // Optional, if you want bounded box UI
+                    videoConstraints: { facingMode: "environment" }
+                },
+                (decodedText, decodedResult) => {
+                    // do something when code is read
+                },
+                (errorMessage) => {
+                    // parse error, ignore it.
+                })
+                .catch((err) => {
+                    // Start failed, handle it.
+                });
+        }
+    }).catch(err => {
+        // handle err
+    });
 }
 
 const onScanSuccess = (decodedText, decodedResult) => {
