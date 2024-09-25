72fb72020ebd4ce8509f12e5896259851583c350
Ly Phu <phuly@bravesoft.com.vn>
2024-09-25 16:51:02 +0700

    update

--- src/views/QRView.vue
+++ src/views/QRView.vue
@@ -13,7 +13,7 @@
                     <img src="../assets/camera-switch.svg" alt="switch camera" />
                 </button>
                 <div class="qr-box">
-                    
+                    <qrcode-stream @detect="onDetect"></qrcode-stream>
                 </div>
             </qrcode-stream>
         </div>
